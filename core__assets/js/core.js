aae7f9a65b8755acfb371c903e0a62859cd502ca
Jo-Nikko Liawan <nikko.qudos@gmail.com>
2019-12-12 14:58:25 +0800

    new update fix IOS device

--- core__assets/js/core.js
+++ core__assets/js/core.js
@@ -70,3 +70,65 @@ $(document).ready(function () {
 
     includeHTML();
 });
+
+
+/*==============================
+    Copy to clipboard function
+  ==============================*/
+$(document).ready(function() {
+    $("#c1").click(function(event) {
+        event.preventDefault();
+        CopyToClipboard("QUDOSRIDER25", true, "Copied!");
+    });
+    $("#c2").click(function(event) {
+        event.preventDefault();
+        CopyToClipboard("QUDOSRIDER18", true, "Copied!");
+    });
+    $("#c3").click(function(event) {
+        event.preventDefault();
+        CopyToClipboard("QUDOSRIDER16", true, "Copied!");
+    });
+    $("#c4").click(function(event) {
+        event.preventDefault();
+        CopyToClipboard("QUDOSRIDER15", true, "Copied!");
+    });
+    
+    $('.clip-btn').click(function() {
+        $(this).addClass('active');
+        setTimeout(function(){
+            $('.active').removeClass('active');
+        },2000);
+    }); 
+});
+
+function CopyToClipboard(value, showNotification, notificationText) {
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val(value).select();
+    document.execCommand("copy");
+    $temp.remove();
+
+    if (typeof showNotification === 'undefined') {
+        showNotification = true;
+    }
+    if (typeof notificationText === 'undefined') {
+        notificationText = "Copied to clipboard";
+    }
+
+    var notificationTag = $("div.copy-notification");
+    if (showNotification && notificationTag.length == 0) {
+        notificationTag = $("<div/>", {
+            "class": "copy-notification",
+            text: notificationText
+        });
+        $("body").append(notificationTag);
+
+        notificationTag.fadeIn("slow", function() {
+            setTimeout(function() {
+                notificationTag.fadeOut("slow", function() {
+                    notificationTag.remove();
+                });
+            }, 1000);
+        });
+    }
+}
